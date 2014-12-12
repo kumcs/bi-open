60a84d25af925ea0eda7b65c311b78297c8a8bba
jgunderson <jgunderson@erpbisolutions.com>
2014-12-12 12:00:55 -0500

    access via jdbc

--- scripts/build_bi.sh
+++ scripts/build_bi.sh
@@ -27,6 +27,7 @@ DATABASEUSER=admin
 DATABASEPASSWORD=admin
 DATABASEPORT=5432
 DATABASELOADPORT=5432
+DATABASELOADEMBEDDED=N
 DATABASESSL=
 GETZIPS=Y
 TENANT=default
@@ -36,7 +37,7 @@ HTTPNAME=localhost
 CREATE=Y
 INCREMENTAL=N
 
-while getopts ":ieblmuxyzd:U:g:P:t:N:n:j:h:p:c:o:r:k:" opt; do
+while getopts ":ieblmuxyzEd:U:g:P:t:N:n:j:h:p:c:o:r:k:" opt; do
   case $opt in
     e)
       # Install ErpBI and configure
@@ -81,6 +82,10 @@ while getopts ":ieblmuxyzd:U:g:P:t:N:n:j:h:p:c:o:r:k:" opt; do
     o)
       # Set database port (for load)
       DATABASELOADPORT=$OPTARG
+      ;;
+    E)
+      # Set database (for load) to embedded for demos
+      DATABASELOADEMBEDDED=N
       ;;	  
     y)
       # Set database ssl connect option (for extract)
@@ -129,7 +134,7 @@ while getopts ":ieblmuxyzd:U:g:P:t:N:n:j:h:p:c:o:r:k:" opt; do
     z)
       # Do not download Pentaho zip files
       GETZIPS=N
-      ;;  
+      ;;	  
     \?)
       log "Invalid option: -"$OPTARG
       exit 1
@@ -329,13 +334,18 @@ download_files () {
 	sed s/port=\"8443\"/port=\"$BISERVERPORT\"/ | \
 	sed s/sslProtocol=\"TLS\"/protocols=\"TLSv1.2,TLSv1.1,TLSv1,SSLv2Hello\"/ \
 	> server.xml  2>&1 | tee -a $LOG_FILE
-	
-	cdir $BISERVER_HOME/biserver-ce/tomcat/conf/Catalina/localhost
-	mv pentaho.xml pentaho.xml.sample
-	cat pentaho.xml.sample | \
-	sed s/org.h2.Driver/org.postgresql.Driver/ | \
-	sed s'#jdbc:h2:../../../h2database/demomfg#jdbc:postgresql://localhost:'$DATABASELOADPORT'/erpbi#' \
-	> pentaho.xml  2>&1 | tee -a $LOG_FILE
+	#
+	# If datamart is not embedded update connection info to postgresql
+	#
+	if  [ "$DATABASELOADEMBEDDED" = "N" ]
+      then	
+	    cdir $BISERVER_HOME/biserver-ce/tomcat/conf/Catalina/localhost
+	    mv pentaho.xml pentaho.xml.sample
+	    cat pentaho.xml.sample | \
+	    sed s/org.h2.Driver/org.postgresql.Driver/ | \
+	    sed s'#jdbc:h2:../../../h2database/demomfg#jdbc:postgresql://localhost:'$DATABASELOADPORT'/erpbi#' \
+	    > pentaho.xml  2>&1 | tee -a $LOG_FILE
+	fi
 }
 
 run_scripts() {
@@ -399,8 +409,6 @@ load_pentaho() {
 	sed s'#erpi.source.url=.*#erpi.source.url=jdbc\:postgresql\://'$DATABASEHOST'\:'$DATABASEPORT'/'$DATABASE$DATABASESSL'#' | \
 	sed s'#erpi.source.user=.*#erpi.source.user='$DATABASEUSER'#' | \
 	sed s'#erpi.source.password.*#erpi.source.password='$DATABASEPASSWORD'#' | \
-	sed s'#erpi.datamart.port.*#erpi.datamart.port='$DATABASELOADPORT'#' | \
-	sed s'#erpi.datamart.url=.*#erpi.datamart.url=jdbc\:postgresql\://localhost\:'$DATABASELOADPORT'/erpbi#' | \
 	sed s'#erpi.cities.file.*#erpi.cities.file='$CITIES'#' | \
 	sed s'#erpi.tenant.id=.*#erpi.tenant.id='$TENANT'.'$DATABASE'#' | \
 	#sed s'#erpi.tenant.id=.*#erpi.tenant.id='default.dev'#' |
@@ -408,6 +416,23 @@ load_pentaho() {
 	sed s'#erpi.loaderpath=.*#erpi.loaderpath='$PSGLOCATION'#' | \
 	sed s'#erpi.incremental=.*#erpi.incremental='$INCREMENTAL'#' \
 	> $KETTLE_HOME/.kettle/kettle.properties  2>&1 | tee -a $LOG_FILE
+	
+	if  [ "$DATABASELOADEMBEDDED" = "N" ]
+      then
+	  	mv $KETTLE_HOME/.kettle/kettle.properties $KETTLE_HOME/.kettle/kettle.properties.sample  2>&1 | tee -a $LOG_FILE
+	    cat $KETTLE_HOME/.kettle/kettle.properties.sample | \
+	    sed s'#erpi.datamart.port.*#erpi.datamart.port='$DATABASELOADPORT'#' | \
+	    sed s'#erpi.datamart.url=.*#erpi.datamart.url=jdbc\:postgresql\://localhost\:'$DATABASELOADPORT'/erpbi#' \
+	    > $KETTLE_HOME/.kettle/kettle.properties  2>&1 | tee -a $LOG_FILE
+	  else
+	  	mv $KETTLE_HOME/.kettle/kettle.properties $KETTLE_HOME/.kettle/kettle.properties.sample  2>&1 | tee -a $LOG_FILE
+	    cat $KETTLE_HOME/.kettle/kettle.properties.sample | \
+	    sed s'#erpi.datamart.loader.*#erpi.datamart.loader=jdbc#' | \
+	    sed s'#erpi.datamart.schemacascade.*#erpi.datamart.schemacascade=#' | \
+	    sed s'#erpi.datamart.driver.*#erpi.datamart.driver=org.h2.Driver#' | \
+	    sed s'#erpi.datamart.url=.*#erpi.datamart.url=jdbc\:h2\:../h2database/erpbi#' \
+	    > $KETTLE_HOME/.kettle/kettle.properties  2>&1 | tee -a $LOG_FILE
+	fi	
 	
 	# Set PGPASSWORD to stop psg from prompting for password.  In case pg_hba.conf does not have
 	# local   all   all     trust 
